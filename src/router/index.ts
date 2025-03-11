fa727b3839fddd874b43563c011aafcfd9e26a0c
huiyu.tonia <huiyu.tonia.chang@gmail.com>
2025-03-11 14:06:56 +0800

    updated: base path test

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import { useAuthStore } from '@/stores/auth'
-
+// import.meta.env.BASE_URL
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory('/team/'),
   routes: [
     {
       path: '/',
