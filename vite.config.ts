50b2830ef67e48b9ac9ee9bc528ad882943e4cdb
huiyu.tonia <huiyu.tonia.chang@gmail.com>
2025-03-11 14:31:11 +0800

    feat: updated routes

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
-  base: './',
+  base: '/',
   server: {
     port: 3035,
   },
